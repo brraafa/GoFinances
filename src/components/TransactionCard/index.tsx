15e753e8ab716df9921338a4e56fb53dff068ffa
Rafael França <apnxrafa@gmail.com>
2022-02-11 18:36:59 -0300

    test

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -36,7 +36,7 @@ export function TransactionCard({ data } : Props){
             </Title>
 
             <Amount type={data.type}>
-                { data.type === 'negative' & '- ' }
+                { data.type === 'negative' && '- ' }
                 {data.amount}
             </Amount>
 
